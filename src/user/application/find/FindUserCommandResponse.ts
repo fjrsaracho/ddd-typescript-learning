b677b5eb3516421f9123c8fb04e04965613c429b
Javier Ruiz <fjrsaracho@gmail.com>
2023-10-02 16:58:54 +0200

    remove uneeded undefined

--- src/user/application/find/FindUserCommandResponse.ts
+++ src/user/application/find/FindUserCommandResponse.ts
@@ -1,9 +1,9 @@
 import User from "../../domain/entity/User";
 
 export default class FindUserCommandResponse {
-    users: { name: string; id: string; lastname: string }[] | undefined;
+    users: { name: string; id: string; lastname: string }[];
 
-    constructor(users?: { id: string; name: string; lastname: string }[]) {
+    constructor(users: { id: string; name: string; lastname: string }[]) {
         this.users = users;
     }
 }
